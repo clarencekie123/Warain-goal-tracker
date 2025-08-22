10ad189620b467a550a73d641f2e6117689c25f4
“N1N25” <clarenceninowarain@gmail.com>
2025-08-22 16:52:46 +0800

    the color has changed

--- app/index.jsx
+++ app/index.jsx
@@ -29,7 +29,7 @@ const styles = StyleSheet.create({
   link: {
     marginVertical: 20,
     padding: 16,
-    backgroundColor: '#21cc8d',
+    backgroundColor: '#1c74e6ff',
     color: 'white',
     borderRadius: 8,
   },
